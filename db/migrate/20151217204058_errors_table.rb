89f6d6a3c98dc7e6cc68d2bbd9232c8850f6a66f
Carrick Rogers <carrickr@iu.edu>
2015-12-17 15:46:23 -0500

    errors table

--- db/migrate/20151217204058_errors_table.rb
+++ db/migrate/20151217204058_errors_table.rb
@@ -1,4 +1,10 @@
 class ErrorsTable < ActiveRecord::Migration
   def change
+    create_table :errors do |t|
+      t.string :mco_id
+      t.string :error_time
+      t.string :error_message
+      t.string :error_code
+    end
   end
 end
