781fe12c932f547100bf1c2cfad169b9c53a09d6
Carrick Rogers <carrickr@iu.edu>
2015-12-20 14:28:02 -0500

    new media objects migration

---
+++ db/migrate/20151220192108_media_objects.rb
@@ -0,0 +1,16 @@
+class MediaObjects < ActiveRecord::Migration
+  def change
+    create_table :media_objects do |t|
+      t.string :group_name
+      t.string :status
+      t.boolean :error
+      t.string :error_message
+      t.string :created
+      t.string :last_modified
+      t.string :avalon_chosen
+      t.string :avalon_pid
+      t.string :avalon_url
+      t.boolean :locked
+    end
+  end
+end
