ea46c87889ae7aca5e47f3b05fe64d4e9fe64f79
Carrick Rogers <carrickr@iu.edu>
2015-12-23 14:01:27 -0500

    test for writing errors

--- spec/models/media_object_spec.rb
+++ spec/models/media_object_spec.rb
@@ -215,9 +215,19 @@ describe 'creation of media objects' do
           @media_object.get_object_collection_id(@object, 'target')
         end
       end
+    end
+  end
 
-
-
+  describe 'recording errors' do
+    it 'writes an error to the database' do
+      #expect(@media_object).to receive(:update_status).at_least(:once)
+      content = @media_object.parse_request_body(load_sample_obj)
+      @media_object.destroy_object(content[:json][:group_name])
+      @media_object.register_object(content)
+      expect(@media_object.object_status_as_json(content[:json][:group_name])['error']).to be_falsey
+      @media_object.object_error_and_exit(content[:json], 'test error')
+      expect(@media_object.object_status_as_json(content[:json][:group_name])['error']).to be_truthy
+      expect(@media_object.object_status_as_json(content[:json][:group_name])['message']).to eq('test error')
 
     end
   end
