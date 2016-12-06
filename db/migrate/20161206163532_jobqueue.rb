f9cade333cb8f2a8c4241d92986001b2309d6b39
Carrick Rogers <carrickr@iu.edu>
2016-12-06 12:55:30 -0500

    rubocop offense removal

--- db/migrate/20161206163532_jobqueue.rb
+++ db/migrate/20161206163532_jobqueue.rb
@@ -1,5 +1,5 @@
 class Jobqueue < ActiveRecord::Migration
   def change
-    add_column :media_objects, :api_hash, :text, limit: 4294967295
+    add_column :media_objects, :api_hash, :text, limit: 4_294_967_295
   end
 end
