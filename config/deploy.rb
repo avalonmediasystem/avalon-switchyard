9745f0885fad651d9a704dfce0202281b3cdfe2a
Phil Dinh <phuongdh@gmail.com>
2017-10-31 13:21:39 -0400

    Update cap script for CI deploy  (#51)
    * Update deploy script for CI
    
    * Bundle without
    
    * Update Gemfile
    
    Travis to exclude production
    
    Add dotenv Gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,6 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+# lock '3.4.0'
 
-
-set :user, ask('Username', 'enter the username for the server')
-set :deploy_host, ask('Server', 'enter in the server you are deploying to')
-set :application, ask('Appname', 'enter the name of the app')
-set :repo_url, 'git@github.com:avalonmediasystem/avalon-switchyard.git'
-# Default branch is :master
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
